ea676249bf34fa7f455bdb0ee3db35eabe6060cf
Jorge Aguirre <jorge.aguirre.leon@gmail.com>
2017-10-25 00:06:07 +0200

    Added location

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import { View, Platform, StatusBar } from 'react-native'
-import AddEntry from './components/AddEntry'
-import History from './components/History'
-import {createStore} from 'redux'
-import {Provider} from 'react-redux'
-import reducer from './reducers'
-import {TabNavigator, StackNavigator} from 'react-navigation'
-import {purple, white} from './utils/colors'
-import { FontAwesome, Ionicons} from '@expo/vector-icons'
-import { Constants } from 'expo'
-import EntryDetail from './components/EntryDetail'
+import React                              from 'react'
+import { View, Platform, StatusBar }      from 'react-native'
+import {createStore}                      from 'redux'
+import {Provider}                         from 'react-redux'
+import reducer                            from './reducers'
+import {TabNavigator, StackNavigator}     from 'react-navigation'
+import {purple, white}                    from './utils/colors'
+import { FontAwesome, Ionicons}           from '@expo/vector-icons'
+import { Constants }                      from 'expo'
+import AddEntry                           from './components/AddEntry'
+import History                            from './components/History'
+import EntryDetail                        from './components/EntryDetail'
+import Live                               from './components/Live'
 
 function UdaciStatusBar({backgroundColor, ...props}) {
   return(
@@ -34,6 +35,13 @@ const Tabs = TabNavigator({
       tabBarIcon: ({tintColor})=> <FontAwesome name='plus-square' size={30} color={tintColor} />
     }
   },
+  Live: {
+    screen: Live,
+    navigationOptions: {
+      tabBarLabel: 'Live',
+      tabBarIcon: ({tintColor})=> <Ionicons name='ios-speedometer' size={30} color={tintColor} />
+    }
+  }
 }, {
   navigationOptions: {
     header: null
